cced622fa1b66e53b0f4e6b2bb5ff5007d190101
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-11-21 16:52:00 -0300

    feat: design of home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from 'next'
+import Image from 'next/image'
+import { SubscribeButton } from '../components/subscribe-button'
+
+import styles from './home.module.scss'
 
 const Home: NextPage = () => {
   return (
-    <h1>Bom dia</h1>
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world.</h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+      <Image width={336} height={521} src="/images/avatar.svg" alt="Girl coding" />
+    </main>
   )
 }
 
