ae63a4de99832a99225c621cfda3decc6a07c618
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-11-27 14:49:35 -0300

    feat: register login informations into faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from 'next-auth'
+import { query as q } from 'faunadb'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   providers: [
@@ -9,5 +12,42 @@ export default NextAuth({
       scope: 'read:user',
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNING_KEY || '',
+  },
+  callbacks: {
+    async signIn(user, account) {
+      const { email } = user
+      const { id } = account
+
+      if (!email || !id) {
+        throw 'Missing informations'
+      }
+
+      try {
+        const matchUserQuery = q.Match(
+          q.Index('user_by_gid'),
+          id,
+        )
+
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(matchUserQuery),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email, gid: id } },
+            ),
+            q.Get(matchUserQuery),
+          )
+        )
+
+        return true
+      } catch {
+        return false
+      }
+    }
+  },
 })
  
