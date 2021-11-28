fd1012775c08ef8d90ccae5ff52fadb87a4ff08d
Jonatan Frederico <jonatanfrederico@gmail.com>
2021-11-27 21:48:29 -0300

    feat: add links into header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,29 @@
+import { useRouter } from 'next/router'
 import Image from 'next/image'
+import Link from 'next/link'
 
 import { SignInButton } from '../sign-in-button'
 import styles from './styles.module.scss'
 
 export function Header() {
+  const route = useRouter()
+
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <Image width={100} height={30} src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href='/'>
+            <a className={route.pathname === '/' ? styles.active : ''}>
+              Home
+            </a>
+          </Link>
+          <Link href='/posts'>
+            <a className={route.pathname.includes('/posts') ? styles.active : ''}>
+              Posts
+            </a>
+          </Link>
         </nav>
 
         <SignInButton />
