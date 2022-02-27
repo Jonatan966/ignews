eb6e997c717ba8b57f63296b9d9d3c3c8997d31a
Jonatan Frederico <jonatanfrederico@gmail.com>
2022-02-27 19:32:28 -0300

    feat: add and config jest

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  testPathIgnorePatterns: ["/node_modules", "/.next/"],
+  setupFilesAfterEnv: [
+    "<rootDir>/src/tests/setupTests.ts",
+  ],
+  transform: { 
+    //transformar códigos dos arquivos antes de executar os testes
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
+  },
+  moduleNameMapper: {
+    "\\.(scss|css|sass)$": "identity-obj-proxy"
+  },
+  testEnvironment: "jsdom", //cria uma representação da dom em Javascript
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+  ],
+  coverageReporters: ["lcov", "json"],
+}
